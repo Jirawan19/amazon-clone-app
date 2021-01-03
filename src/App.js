0b6b09247f57a405048a79236d434b73b1eb60a3
nam <nam.jirawan@icloud.com>
2021-01-03 15:33:59 +0700

    clip 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+
 import './App.css';
 import Header from "./Header"
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Login from "./Login"
+import Home from "./Home"
 
 function App() {
     return (
@@ -14,6 +16,10 @@ function App() {
                     <Route path="/login">
                         <Login/>
                     </Route>
+                    <Router path="/">
+                        <Header/>
+                        <Home/>
+                    </Router>
                 </Switch>
             </div>
         </Router>
